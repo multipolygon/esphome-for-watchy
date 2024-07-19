acb98388be5b5510358b51405b13a576eb7c413a
multipolygon <multipolygon@multipolygon.net>
2024-07-19 15:22:35 +1000

    clean up icons

---
+++ generate-mdi.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+const required = fs
+  .readFileSync("./watchy.yaml")
+  .toString()
+  .match(/"\$mdi\w+?"/g)
+  .map((x) => x.replaceAll('"', "").replaceAll("$", ""));
+
+console.log(required.length, "icons required");
+
+function camelize(str) {
+  return str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    })
+    .replace(/\s+/g, "");
+}
+
+const x = fs
+  .readFileSync("fonts/mdi/scss/_variables.scss")
+  .toString()
+  .split("(")[1]
+  .split(")")[0]
+  .trim()
+  .split("\n")
+  .map((l) => l.trim().replaceAll('"', "").replaceAll(",", "").replaceAll("-", " ").split(": "))
+  .map(([name, code]) => [camelize("mdi " + name), code])
+  .filter(([name, code]) => required.includes(name));
+
+console.log(x.length);
+
+fs.writeFileSync("./mdi.yaml", x.map(([name, code]) => `${name}: "\\U000${code}"`).join("\n") + "\n");
+fs.writeFileSync("./mdi.h", x.map(([name, code]) => `const char *${name} = "\\U000${code}";`).join("\n") + "\n");
--- weather.h
+++ weather.h
@@ -1,108 +1,144 @@
 #include <map>
-
-static const std::string clear_night = "\U000F0594";
-static const std::string cloudy = "\U000F0590";
-static const std::string haze = "\U000F0F30";
-static const std::string exceptional = "\U000F0026";
-static const std::string fog = "\U000F0591";
-static const std::string hail = "\U000F0592";
-static const std::string lightning = "\U000F0593";
-static const std::string lightning_rainy = "\U000F067E";
-static const std::string partly_cloudy = "\U000F0595";
-static const std::string pouring = "\U000F0596";
-static const std::string rainy = "\U000F0597";
-static const std::string snowy = "\U000F0598";
-static const std::string snowy_rainy = "\U000F067F";
-static const std::string sunny = "\U000F0599";
-static const std::string windy = "\U000F059D";
-static const std::string windy_variant = "\U000F059E";
-
-static const std::string night = "\U000F0594";
-static const std::string night_partly_cloudy = "\U000F0F31";
+#include "mdi.h"
 
 // https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
 
-std::map<int, std::string> weather_icon_map{
-    {0, exceptional},
-
-    {200, lightning_rainy}, // thunderstorm with light rain
-    {201, lightning_rainy}, // thunderstorm with rain
-    {202, lightning_rainy}, // thunderstorm with heavy rain
-    {210, lightning},       // light thunderstorm
-    {211, lightning},       // thunderstorm
-    {212, lightning},       // heavy thunderstorm
-    {221, lightning},       // ragged thunderstorm
-    {230, lightning_rainy}, // thunderstorm with light drizzle
-    {231, lightning_rainy}, // thunderstorm with drizzle
-    {232, lightning_rainy}, // thunderstorm with heavy drizzle
-
-    {300, rainy}, // light intensity drizzle
-    {301, rainy}, // drizzle
-    {302, rainy}, // heavy intensity drizzle
-    {310, rainy}, // light intensity drizzle rain
-    {311, rainy}, // drizzle rain
-    {312, rainy}, // heavy intensity drizzle rain
-    {313, rainy}, // shower rain and drizzle
-    {314, rainy}, // heavy shower rain and drizzle
-    {321, rainy}, // shower drizzle
-
-    {500, rainy},   // light rain
-    {501, pouring}, // moderate rain
-    {502, pouring}, // heavy intensity rain
-    {503, pouring}, // very heavy rain
-    {504, pouring}, // extreme rain
-    {511, hail},    // freezing rain
-    {520, rainy},   // light intensity shower rain
-    {521, rainy},   // shower rain
-    {522, pouring}, // heavy intensity shower rain
-    {531, rainy},   // ragged shower rain
-
-    {600, snowy},       // light snow
-    {601, snowy},       // snow
-    {602, snowy},       // heavy snow
-    {611, snowy},       // sleet
-    {612, snowy},       // light shower sleet
-    {613, snowy},       // shower sleet
-    {615, snowy_rainy}, // light rain and snow
-    {616, snowy_rainy}, // rain and snow
-    {620, snowy},       // light shower snow
-    {621, snowy},       // shower snow
-    {622, snowy},       // heavy shower snow 	13d
-
-    {701, fog},   // mist
-    {711, fog},   // smoke
-    {721, haze},  // haze
-    {731, fog},   // sand/dust whirls
-    {741, fog},   // fog
-    {751, fog},   // sand
-    {761, fog},   // dust
-    {762, fog},   // volcanic ash
-    {771, windy}, // squalls
-    {781, windy}, // tornado
-
-    {800, sunny}, // clear
-
-    {801, partly_cloudy}, // few clouds: 11-25%
-    {802, partly_cloudy}, // scattered clouds: 25-50%
-    {803, partly_cloudy}, // broken clouds: 51-84%
-    {804, cloudy},        // overcast clouds: 85-100%
-
-    {1800, night},               // clear
-    {1801, night_partly_cloudy}, // few clouds: 11-25%
-    {1802, night_partly_cloudy}, // scattered clouds: 25-50%
-    {1803, night_partly_cloudy}, // broken clouds: 51-84%
-    {1804, cloudy},              // overcast clouds: 85-100%
+std::map<int, const char *> weather_icon_map{
+    {0, mdiAlert},                       // Exception
+                                         //// STORM ////
+    {200, mdiWeatherLightningRainy},     // thunderstorm with light rain
+    {201, mdiWeatherLightningRainy},     // thunderstorm with rain
+    {202, mdiWeatherLightningRainy},     // thunderstorm with heavy rain
+    {210, mdiWeatherLightning},          // light thunderstorm
+    {211, mdiWeatherLightning},          // thunderstorm
+    {212, mdiWeatherLightning},          // heavy thunderstorm
+    {221, mdiWeatherLightning},          // ragged thunderstorm
+    {230, mdiWeatherLightningRainy},     // thunderstorm with light drizzle
+    {231, mdiWeatherLightningRainy},     // thunderstorm with drizzle
+    {232, mdiWeatherLightningRainy},     // thunderstorm with heavy drizzle
+                                         //// DRIZZLE ////
+    {300, mdiWeatherRainy},              // light intensity drizzle
+    {301, mdiWeatherRainy},              // drizzle
+    {302, mdiWeatherPouring},            // heavy intensity drizzle
+    {310, mdiWeatherRainy},              // light intensity drizzle rain
+    {311, mdiWeatherRainy},              // drizzle rain
+    {312, mdiWeatherPouring},            // heavy intensity drizzle rain
+    {313, mdiWeatherRainy},              // shower rain and drizzle
+    {314, mdiWeatherPouring},            // heavy shower rain and drizzle
+    {321, mdiWeatherRainy},              // shower drizzle
+                                         //// RAIN ////
+    {500, mdiWeatherPartlyRainy},        // light rain
+    {501, mdiWeatherPartlyRainy},        // moderate rain
+    {502, mdiWeatherPartlyRainy},        // heavy intensity rain
+    {503, mdiWeatherPartlyRainy},        // very heavy rain
+    {504, mdiWeatherPartlyRainy},        // extreme rain
+    {511, mdiWeatherHail},               // freezing rain
+    {520, mdiWeatherRainy},              // light intensity shower rain
+    {521, mdiWeatherRainy},              // shower rain
+    {522, mdiWeatherRainy},              // heavy intensity shower rain
+    {531, mdiWeatherRainy},              // ragged shower rain
+                                         //// SNOW ////
+    {600, mdiWeatherPartlySnowy},        // light snow
+    {601, mdiWeatherSnowy},              // snow
+    {602, mdiWeatherSnowyHeavy},         // heavy snow
+    {611, mdiWeatherSnowyRainy},         // sleet
+    {612, mdiWeatherPartlySnowyRainy},   // light shower sleet
+    {613, mdiWeatherSnowyRainy},         // shower sleet
+    {615, mdiWeatherPartlySnowyRainy},   // light rain and snow
+    {616, mdiWeatherSnowyRainy},         // rain and snow
+    {620, mdiWeatherPartlySnowyRainy},   // light shower snow
+    {621, mdiWeatherSnowyRainy},         // shower snow
+    {622, mdiWeatherSnowyHeavy},         // heavy shower snow 	13d
+                                         //// ATMOSPHERE ////
+    {701, mdiWeatherFog},                // mist
+    {711, mdiWeatherFog},                // smoke
+    {721, mdiWeatherHazy},               // haze
+    {731, mdiWeatherDust},               // sand/dust whirls
+    {741, mdiWeatherFog},                // fog
+    {751, mdiWeatherDust},               // sand
+    {761, mdiWeatherDust},               // dust
+    {762, mdiWeatherHazy},               // volcanic ash
+    {771, mdiWeatherWindy},              // squalls
+    {781, mdiWeatherTornado},            // tornado
+                                         //// SUNNY ////
+    {800, mdiWeatherSunny},              // clear
+    {801, mdiWeatherSunny},              // few clouds: 11-25%
+    {802, mdiWeatherPartlyCloudy},       // scattered clouds: 25-50%
+    {803, mdiWeatherPartlyCloudy},       // broken clouds: 51-84%
+    {804, mdiWeatherCloudy},             // overcast clouds: 85-100%
+                                         /////////// NIGHT ///////////
+                                         //// STORM ////
+    {1200, mdiWeatherLightningRainy},    // thunderstorm with light rain
+    {1201, mdiWeatherLightningRainy},    // thunderstorm with rain
+    {1202, mdiWeatherLightningRainy},    // thunderstorm with heavy rain
+    {1210, mdiWeatherLightning},         // light thunderstorm
+    {1211, mdiWeatherLightning},         // thunderstorm
+    {1212, mdiWeatherLightning},         // heavy thunderstorm
+    {1221, mdiWeatherLightning},         // ragged thunderstorm
+    {1230, mdiWeatherLightningRainy},    // thunderstorm with light drizzle
+    {1231, mdiWeatherLightningRainy},    // thunderstorm with drizzle
+    {1232, mdiWeatherLightningRainy},    // thunderstorm with heavy drizzle
+                                         //// DRIZZLE ////
+    {1300, mdiWeatherRainy},             // light intensity drizzle
+    {1301, mdiWeatherRainy},             // drizzle
+    {1302, mdiWeatherPouring},           // heavy intensity drizzle
+    {1310, mdiWeatherRainy},             // light intensity drizzle rain
+    {1311, mdiWeatherRainy},             // drizzle rain
+    {1312, mdiWeatherPouring},           // heavy intensity drizzle rain
+    {1313, mdiWeatherRainy},             // shower rain and drizzle
+    {1314, mdiWeatherPouring},           // heavy shower rain and drizzle
+    {1321, mdiWeatherRainy},             // shower drizzle
+                                         //// RAIN ////
+    {1500, mdiWeatherRainy},             // light rain
+    {1501, mdiWeatherRainy},             // moderate rain
+    {1502, mdiWeatherRainy},             // heavy intensity rain
+    {1503, mdiWeatherRainy},             // very heavy rain
+    {1504, mdiWeatherRainy},             // extreme rain
+    {1511, mdiWeatherHail},              // freezing rain
+    {1520, mdiWeatherRainy},             // light intensity shower rain
+    {1521, mdiWeatherRainy},             // shower rain
+    {1522, mdiWeatherRainy},             // heavy intensity shower rain
+    {1531, mdiWeatherRainy},             // ragged shower rain
+                                         //// SNOW ////
+    {1600, mdiWeatherSnowy},             // light snow
+    {1601, mdiWeatherSnowy},             // snow
+    {1602, mdiWeatherSnowyHeavy},        // heavy snow
+    {1611, mdiWeatherSnowyRainy},        // sleet
+    {1612, mdiWeatherSnowyRainy},        // light shower sleet
+    {1613, mdiWeatherSnowyRainy},        // shower sleet
+    {1615, mdiWeatherSnowyRainy},        // light rain and snow
+    {1616, mdiWeatherSnowyRainy},        // rain and snow
+    {1620, mdiWeatherSnowyRainy},        // light shower snow
+    {1621, mdiWeatherSnowyRainy},        // shower snow
+    {1622, mdiWeatherSnowyHeavy},        // heavy shower snow 	13d
+                                         //// ATMOSPHERE ////
+    {1701, mdiWeatherFog},               // mist
+    {1711, mdiWeatherFog},               // smoke
+    {1721, mdiWeatherHazy},              // haze
+    {1731, mdiWeatherDust},              // sand/dust whirls
+    {1741, mdiWeatherFog},               // fog
+    {1751, mdiWeatherDust},              // sand
+    {1761, mdiWeatherDust},              // dust
+    {1762, mdiWeatherHazy},              // volcanic ash
+    {1771, mdiWeatherWindy},             // squalls
+    {1781, mdiWeatherTornado},           // tornado
+                                         //// SUNNY ////
+    {1800, mdiWeatherNight},             // clear
+    {1801, mdiWeatherNight},             // few clouds: 11-25%
+    {1802, mdiWeatherNightPartlyCloudy}, // scattered clouds: 25-50%
+    {1803, mdiWeatherNightPartlyCloudy}, // broken clouds: 51-84%
+    {1804, mdiWeatherCloudy},            // overcast clouds: 85-100%
 };
 
 int weatherCode(int code, const std::string &daynight)
 {
   // ESP_LOGE("cal", "%d, %s, %c", code, icon, icon.back());
-  if (code >= 800 && code < 900 && !daynight.empty() && daynight.back() == 'n')
+  if (!daynight.empty() && daynight.back() == 'n')
   {
     return 1000 + code;
   }
   return code;
-}
+};
 
 int getWeatherCodeIndex(int quadrant, int h24)
 {
@@ -125,7 +161,7 @@ int getWeatherCodeIndex(int quadrant, int h24)
     offset = 1;
   }
   return (offset + quadrant) % 4;
-}
+};
 
 int rotateWeatherCode(int quadrant, int h24, int a, int b, int c, int d)
 {
@@ -147,20 +183,92 @@ int rotateWeatherCode(int quadrant, int h24, int a, int b, int c, int d)
     return d;
   }
   return 0;
-}
+};
 
 int getClkXY(int hour, int xy)
 {
   double hr = (hour / 12.0 * 360.0 - 90.0) * (M_PI / 180.0);
   return 100 + 90 * (xy == 0 ? cos(hr) : sin(hr));
-}
+};
 
 int getClkX(int hour)
 {
   return getClkXY(hour, 0);
-}
+};
 
 int getClkY(int hour)
 {
   return getClkXY(hour, 1);
-}
+};
+
+const char *sky[] = {
+    mdiStarOutline,
+    mdiStarShootingOutline,
+    mdiCreationOutline,
+    mdiHelicopter,
+    mdiAirballoon,
+    mdiBird,
+    mdiSpaceInvaders,
+    mdiSpaceStation,
+    mdiUfoOutline,
+    mdiSatelliteVariant};
+const int skyLen = 10;
+
+const char *city[] = {
+    mdiCity,
+    mdiCityVariant,
+    mdiOfficeBuilding,
+    mdiCity,
+    mdiCityVariant,
+    mdiOfficeBuilding,
+    mdiCity,
+    mdiCityVariant,
+    mdiCityVariant,
+    mdiOfficeBuilding,
+    mdiCity,
+    mdiCity,
+    mdiCityVariant,
+    mdiOfficeBuilding,
+    mdiCity,
+    mdiCityVariant,
+    mdiOfficeBuilding,
+    mdiCity,
+    mdiCityVariant,
+    mdiOfficeBuilding,
+    mdiCastle,
+    mdiGoogleDownasaur,
+    mdiHospitalBuilding,
+    mdiPineTree,
+};
+const int cityLen = 24;
+
+std::map<int, const char *>
+    calendarIcons{
+        // https://www.timeanddate.com/holidays/world/
+        // https://www.timeanddate.com/holidays/un/
+        {101, mdiPartyPopper},
+        {121, mdiSchool},
+        {214, mdiHeart},
+        {229, mdiDebugStepOver},
+        {303, mdiElephant},
+        {320, mdiEmoticonHappyOutline},
+        {320, mdiEmoticonHappyOutline},
+        {321, mdiForest},
+        {322, mdiWater},
+        {401, mdiDonkey},
+        {412, mdiRocketLaunch},
+        {422, mdiEarth},
+        {504, mdiDeathStarVariant},
+        {508, mdiPlusThick},
+        {512, mdiHospitalBuilding},
+        {520, mdiBee},
+        {601, mdiHumanMaleChild},
+        {603, mdiBicycle},
+        {620, mdiFerry},
+        {823, mdiLinkBoxVariant},
+        {915, mdiVote},
+        {921, mdiPeace},
+        {1005, mdiHumanMaleBoard},
+        {1031, mdiHalloween},
+        {1120, mdiTeddyBear},
+    };
